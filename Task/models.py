ed3ba8ad925e0523828c4dd43f892a781607b89a
Arjun_Lubana <arjunlubana562@gmail.com>
2020-09-04 18:22:48 +0300

    Created models for the Database

--- Task/models.py
+++ Task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Subtask(models.Model):
+    title = models.CharField(max_length=500)
+    status = models.BooleanField(default=False)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
