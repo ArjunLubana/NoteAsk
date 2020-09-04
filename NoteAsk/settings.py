e2e287fe8de4e2a2d812014d4179af3afee55eb7
Arjun_Lubana <arjunlubana562@gmail.com>
2020-09-04 10:53:04 +0300

    Created routing for Task and Note apps

--- NoteAsk/settings.py
+++ NoteAsk/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Task',
+    'Note',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'NoteAsk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates',
+                 BASE_DIR / 'templates'/'Note',
+                 BASE_DIR / 'templates'/'Task', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
